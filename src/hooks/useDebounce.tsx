e4997a5592d4b1c2fb0cfdad2c68140c183e1a8e
lisq <mail@lishunqi.com>
2022-02-28 12:33:55 +0800

    删除console.log

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -3,7 +3,6 @@ import {useEffect, useState} from "react";
 function useDebounce(value: any, delay = 300) {
     const [debounce, setDebounce] = useState(value);
     useEffect(() => {
-        console.log('触发3');
         const timer = setTimeout(() => {
             setDebounce(value)
         }, delay)
